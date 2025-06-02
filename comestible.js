1438e0ec6c2f0289f7e5841f88ef7c78fa5cd379
Bloor11 <bloor112005@gmail.com>
2025-06-02 18:49:48 -0500

    Actualizado

--- comestible.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Comestible = void 0;
-class Comestible {
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
-}
-exports.Comestible = Comestible;
